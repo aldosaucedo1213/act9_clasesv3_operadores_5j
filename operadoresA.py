133c9165dd9e8f30c5a0269e74251d63c3fd08b0
aldosaucedo1213 <a.22308051281310@cbtis128.edu.mx>
2024-09-20 09:40:53 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,61 @@
+print("act 9 clases v3")
+print("aldo saucedo mat:22308051281310")
+# zona de class
+class Operadores1310:
+    def suma1310(self,a,s):
+        s1310=a+s
+        print(f"la suma de {a} + {s} = {s1310}")
+
+    def resta1310(self,c,y):
+        s1310=c-y
+        print(f"la resta de {c} - {y} = {s1310}")
+
+    def multiplicacion1310(self,e,r):
+        s1310=e*r
+        print(f"la multiplicacion de {e} * {r} = {s1310}")
+
+    def division1310(self,h,j):
+        s1310=h/j
+        print(f"la division de {h} / {j} = {s1310}")
+
+    def modulo1310(self,f,v):
+        s1310=f%v
+        print(f"la modulo de {f} % {v} = {s1310}")
+
+    def exponente1310(self,o,p):
+        s1310=o**p
+        print(f"la exponente de {o} ** {p} = {s1310}")
+
+    def cociente1310(self,z,x):
+        s1310=z//x
+        print(f"la cociente de {z} // {x} = {s1310}")
+    
+# zona de creacion del objeto 
+operasaucedo=Operadores1310()
+operasaucedo=Operadores1310()
+operasaucedo=Operadores1310()
+operasaucedo=Operadores1310()
+operasaucedo=Operadores1310()
+operasaucedo=Operadores1310()
+operasaucedo=Operadores1310()
+# zona de uso de objetos 
+print("funcion suma ")
+operasaucedo.suma1310(1,7)
+
+print("funcion resta ")
+operasaucedo.resta1310(7,2)
+
+print("funcion multiplicacion ")
+operasaucedo.multiplicacion1310(3,2)
+
+print("funcion division ")
+operasaucedo.division1310(5,4)
+
+print("funcion modulo ")
+operasaucedo.modulo1310(7,8)
+
+print("funcion exponente ")
+operasaucedo.exponente1310(5,9)
+
+print("funcion cociente ")
+operasaucedo.cociente1310(5,3)
